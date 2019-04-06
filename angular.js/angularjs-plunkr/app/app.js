dafa2ef0b694c0824910a33a8e6ac5fb769a5235
Patrick <ppevans11@gmail.com>
2019-04-06 13:56:35 +1300

    added angular.js/angularjs-plunkr

---
+++ angular.js/angularjs-plunkr/app/app.js
@@ -0,0 +1,21 @@
+
+/**
+ *  app/app.js
+ */
+
+//"use strict";
+
+//var angular = require('angular');
+//import angular from 'angular';
+
+// create trackrApp root angular module and MainCtrl controller 
+angular.module('trackrApp', [])
+.controller('MainCtrl', function($scope) {
+    
+    $scope.name = 'TRACKR';
+
+});
+
+
+/* EOF */
+
