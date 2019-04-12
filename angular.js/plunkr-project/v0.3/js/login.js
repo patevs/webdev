ba241b373b4be5bbb8a7ddd3479da0a536ab4135
Patrick <ppevans11@gmail.com>
2019-04-13 10:59:32 +1200

    ✔️  added plunkr-project/v0.3

---
+++ angular.js/plunkr-project/v0.3/js/login.js
@@ -0,0 +1,48 @@
+
+/**
+ *  js/login.js
+ */
+
+angular.module('trackrApp')
+.controller('LoginCtrl', [ '$scope', '$uibModalInstance' , '$window', 'Auth', //, '$state'
+function($scope, $modalInstance, $window, Auth ) { //, $state
+	$scope.credentials = {};
+	$scope.loginForm = {};
+	$scope.error = false;
+	
+	//when the form is submitted
+	$scope.submit = function() {
+		$scope.submitted = true;
+		if (!$scope.loginForm.$invalid) {
+			$scope.login($scope.credentials);
+		} else {
+			$scope.error = true;
+			return;
+		}
+	};
+
+	//Performs the login function, by sending a request to the server with the Auth service
+	$scope.login = function(credentials) {
+		$scope.error = false;
+		Auth.login(credentials, function(user) {
+			//success function
+			$modalInstance.close();
+			//$state.go('home');
+		}, function(err) {
+			console.log("error");
+			$scope.error = true;
+		});
+	};
+	
+	// if a session exists for current user (page was refreshed)
+	// log him in again
+	if ($window.sessionStorage["userInfo"]) {
+		var credentials = JSON.parse($window.sessionStorage["userInfo"]);
+		$scope.login(credentials);
+	}
+
+}]);
+
+
+/* EOF */
+
