1047b1a8b1844450b0cd3407479a792cd5378bc0
Patrick <ppevans11@gmail.com>
2019-04-16 16:23:14 +1200

    fixing more linting errors

--- angular.js/plunkr-project/app/js/login.js
+++ angular.js/plunkr-project/app/js/login.js
@@ -3,45 +3,53 @@
  *  js/login.js
  */
 
+"use strict";
+
 angular.module('trackrApp')
 .controller('LoginCtrl', [ '$scope', '$uibModalInstance' , '$window', 'Auth', //, '$state'
-function($scope, $modalInstance, $window, Auth ) { //, $state
-	$scope.credentials = {};
-	$scope.loginForm = {};
-	$scope.error = false;
+	function($scope, $modalInstance, $window, Auth ) { //, $state
+
+		// view-model
+		var vm = this;
+		vm.scope = $scope;
+
+		vm.scope.credentials = {};
+		vm.scope.loginForm = {};
+		vm.scope.error = false;
 	
-	//when the form is submitted
-	$scope.submit = function() {
-		$scope.submitted = true;
-		if (!$scope.loginForm.$invalid) {
-			$scope.login($scope.credentials);
-		} else {
-			$scope.error = true;
-			return;
+		//when the form is submitted
+		vm.scope.submit = function() {
+			vm.scope.submitted = true;
+			if (!vm.scope.loginForm.$invalid) {
+				vm.scope.login($scope.credentials);
+			} else {
+				vm.scope.error = true;
+				return;
+			}
+		};
+
+		//Performs the login function, by sending a request to the server with the Auth service
+		vm.scope.login = function(credentials) {
+			vm.scope.error = false;
+			Auth.login(credentials, function(user) {
+				//success function
+				$modalInstance.close();
+				//$state.go('home');
+			}, function(err) {
+				//console.log("error");
+				vm.scope.error = true;
+			});
+		};
+	
+		// if a session exists for current user (page was refreshed)
+		// log him in again
+		if ($window.sessionStorage.userInfo) {
+			var credentials = angular.toJson($window.sessionStorage.userInfo);
+			vm.scope.login(credentials);
 		}
-	};
 
-	//Performs the login function, by sending a request to the server with the Auth service
-	$scope.login = function(credentials) {
-		$scope.error = false;
-		Auth.login(credentials, function(user) {
-			//success function
-			$modalInstance.close();
-			//$state.go('home');
-		}, function(err) {
-			console.log("error");
-			$scope.error = true;
-		});
-	};
-	
-	// if a session exists for current user (page was refreshed)
-	// log him in again
-	if ($window.sessionStorage["userInfo"]) {
-		var credentials = JSON.parse($window.sessionStorage["userInfo"]);
-		$scope.login(credentials);
 	}
-
-}]);
+]);
 
 
 /* EOF */
--- angular.js/plunkr-project/app/js/session.js
+++ angular.js/plunkr-project/app/js/session.js
@@ -3,13 +3,17 @@
  *  js/session.js
  */
 
+"use strict";
+
 /*
  * In this service the user data is defined for the current session. Within
  * angular current session is until the page is refreshed. When the page is
  * refreshed the user is reinitialized through $window.sessionStorage at the
  * login.js file.
  */
-angular.module('trackrApp').service('Session', function($rootScope, USER_ROLES) {
+/*
+angular.module('trackrApp')
+.service('Session', function($rootScope, USER_ROLES) {
 
 	this.create = function(user) {
 		this.user = user;
@@ -23,6 +27,20 @@ angular.module('trackrApp').service('Session', function($rootScope, USER_ROLES) 
 	
 	return this;
 	
+});
+*/
+
+angular.module('trackrApp')
+.factory('Session', function($rootScope, USER_ROLES) {
+	this.create = function(user) {
+		this.user = user;
+		this.userRole = user.userRole;
+	};
+	this.destroy = function() {
+		this.user = null;
+		this.userRole = null;
+	}
+	return this;
 });
 
 
