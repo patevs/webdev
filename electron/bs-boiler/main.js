2bdaeac3f88e18d03196cb678dbf6fea4f158d63
patevs <ppevans11@gmail.com>
2019-06-14 15:32:06 +1200

    ✔️  Added electron-reloader dev dependency

--- electron/bs-boiler/main.js
+++ electron/bs-boiler/main.js
@@ -10,6 +10,12 @@ const { app, BrowserWindow } = require("electron");
 
 // require('electron-reload')(__dirname);
 
+// Enable live reloading in development
+try {
+	require('electron-reloader')(module);
+} catch (_) { }
+
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
@@ -66,3 +72,5 @@ app.on("activate", () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+// EOF //
