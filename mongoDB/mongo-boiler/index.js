11bbff17966c4211e444ece3dad71cee1efabab7
patevs <ppevans11@gmail.com>
2019-06-22 17:06:55 +1200

    ✨  Minor updates

--- mongoDB/mongo-boiler/index.js
+++ mongoDB/mongo-boiler/index.js
@@ -23,6 +23,19 @@ const dbName = "myproject";
  * * FUNCTIONS *
  ***************/
 
+const insertDocuments = function(db, callback) {
+	// Get the documents collection
+	const collection = db.collection("documents");
+	// Insert some documents
+	collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }], function(err, result) {
+		assert.equal(err, null);
+		assert.equal(3, result.result.n);
+		assert.equal(3, result.ops.length);
+		console.log("Inserted 3 documents into the collection");
+		callback(result);
+	});
+};
+
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, client) {
 	assert.equal(null, err);
@@ -30,7 +43,9 @@ MongoClient.connect(url, function(err, client) {
 
 	const db = client.db(dbName);
 
-	client.close();
+	insertDocuments(db, function() {
+		client.close();
+	});
 });
 
 /*
