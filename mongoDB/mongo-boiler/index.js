27edf5ab9e98e3313e84f2af38d2d88a9934a2c6
patevs <ppevans11@gmail.com>
2019-06-22 17:02:42 +1200

    ✨  Updates to scripts

--- mongoDB/mongo-boiler/index.js
+++ mongoDB/mongo-boiler/index.js
@@ -4,6 +4,36 @@
 
 "use strict";
 
+/*************
+ * * IMPORTS *
+ *************/
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+
+/***************
+ * * CONSTANTS *
+ ***************/
+// Connection URL
+const url = "mongodb://localhost:27017";
+
+// Database Name
+const dbName = "myproject";
+
+/***************
+ * * FUNCTIONS *
+ ***************/
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+	assert.equal(null, err);
+	console.log("Connected successfully to server");
+
+	const db = client.db(dbName);
+
+	client.close();
+});
+
+/*
 module.exports = (input, options = {}) => {
 	if (typeof input !== "string") {
 		throw new TypeError(`Expected a string, got ${typeof input}`);
@@ -11,5 +41,6 @@ module.exports = (input, options = {}) => {
 
 	return input + " & " + (options.postfix || "rainbows");
 };
+*/
 
 // EOF //
