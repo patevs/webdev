26256c548ff59a4e81e16825a330293aa67a0917
patevs <ppevans11@gmail.com>
2019-06-24 13:55:58 +1200

    ✨  Updates to cli-app/

--- cli-app/cli.js
+++ cli-app/cli.js
@@ -10,47 +10,62 @@
  * * IMPORTS *
  *************/
 const inquirer = require("inquirer");
+const chalk = require("chalk");
 // const meow = require("meow");
 // const cliApp = require(".");
 
-
-console.log("Hi, welcome to Node Pizza");
+// Chalk Theme
+const log = console.log;
+const title = chalk.bold.underline.green;
 
 /***************
  * * FUNCTIONS *
  ***************/
-inquirer
-	.prompt([
-		/* Pass your questions in here */
-		{
-			type: "confirm",
-			name: "toBeDelivered",
-			message: "Is this for delivery?",
-			default: false
-		},
-	])
-	.then(answers => {
-		// Use user feedback for... whatever!!
-		console.log("\nOrder receipt:");
-		console.log(JSON.stringify(answers, null, "  "));
-	});
+
+function cli() {
+	inquirer
+		.prompt([
+			/* Pass your questions in here */
+			{
+				type: "confirm",
+				name: "toBeDelivered",
+				message: "Is this for delivery?",
+				default: false
+			},
+		])
+		.then(answers => {
+			// Use user feedback for... whatever!!
+			console.log("\nOrder receipt:");
+			console.log(JSON.stringify(answers, null, "  "));
+		});
+}
+
+/**
+ * Application entry point
+ */
+(function () {
+	log(title("\nWELCOME TO CLI-APP\n"));
+	cli();
+})();
 
 /*
 const cli = meow(`
 	Usage
-	  $ cli-app [input]
+		$ cli-app [input]
 
 	Options
-	  --foo  Lorem ipsum [Default: false]
+		--foo  Lorem ipsum [Default: false]
 
 	Examples
-	  $ cli-app
-	  unicorns & rainbows
-	  $ cli-app ponies
-	  ponies & rainbows
+		$ cli-app
+		unicorns & rainbows
+		$ cli-app ponies
+		ponies & rainbows
 `);
 
 console.log(cliApp(cli.input[0] || "unicorns"));
 */
+
+
 
 // EOF //
