7e5a8edb2177b47c2a6426bea166121e0205a9c3
PatEvs <ppevans11@gmail.com>
2019-06-28 18:18:15 +1200

    ✨  Updates to index.js

--- webscrapper/index.js
+++ webscrapper/index.js
@@ -4,6 +4,41 @@
 
 "use strict";
 
+/*************
+ * * IMPORTS *
+ *************/
+
+// const axios = require("axios");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const path = require("path");
+
+const handlebars = require("express-handlebars");
+
+/***************
+ * * FUNCTIONS *
+ ***************/
+
+app.engine(".hbs", handlebars({ extname: ".hbs" }));
+
+app.set("PORT", PORT);
+
+app.use(express.static(path.join(__dirname, "assets")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", ".hbs");
+
+app.get("/", function (req, res) {
+	res.render("index", { title: "Jobby" })
+});
+
+app.listen(app.get("PORT"), function () {
+	console.log("Express started on http://localhost:" +
+		app.get("PORT") + "; press Ctrl-C to terminate.");
+});
+
+
+/*
 module.exports = (input, options = {}) => {
 	if (typeof input !== "string") {
 		throw new TypeError(`Expected a string, got ${typeof input}`);
@@ -11,7 +46,7 @@ module.exports = (input, options = {}) => {
 
 	return input + " & " + (options.postfix || "rainbows");
 };
-
+*/
 
 // EOF //
 
