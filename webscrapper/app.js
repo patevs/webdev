fb1ce91978163381393bf64412bf18127bdb5eea
PatEvs <ppevans11@gmail.com>
2019-06-28 18:28:08 +1200

    📦  Updates and improvements

--- webscrapper/app.js
+++ webscrapper/app.js
@@ -1,5 +1,5 @@
 /**
- * `index.js`
+ * `app.js`
  */
 
 "use strict";
@@ -8,7 +8,7 @@
  * * IMPORTS *
  *************/
 
-// const axios = require("axios");
+const axios = require("axios");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -29,7 +29,20 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", ".hbs");
 
 app.get("/", function (req, res) {
-	res.render("index", { title: "Jobby" });
+	// res.render("index", { title: "Jobby" });
+	let url = "https://indreed.herokuapp.com/api/jobs?q=web+developer&limit=50";
+	axios({
+		method: "get",
+		url
+	})
+		.then(function (response) {
+			let jobs = response.data;
+			res.render("index", { title: "Jobby", jobs: jobs });
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+
 });
 
 app.listen(app.get("PORT"), function () {
