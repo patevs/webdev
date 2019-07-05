ae8c40293b9ea710a020de811534da49151a0f26
PatEvs <ppevans11@gmail.com>
2019-07-05 20:02:17 +1200

    ✨  Created flickr/

---
+++ flickr/index.js
@@ -0,0 +1,29 @@
+
+/**
+ * 	* `index.js`
+ */
+
+"use strict";
+
+/*
+module.exports = (input, {postfix = "rainbows"} = {}) => {
+	if (typeof input !== "string") {
+		throw new TypeError(`Expected a string, got ${typeof input}`);
+	}
+
+	return `${input} & ${postfix}`;
+};
+*/
+
+/**
+ *
+ */
+(function () {
+	//..
+	console.log("HELLO!");
+	//..
+})();
+
+
+// EOF //
+
