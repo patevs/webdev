44edfdc0022e232c073a05fe1a7146156105f7e1
PatEvs <ppevans11@gmail.com>
2019-07-25 20:35:26 +1200

    ✨  Updates

--- cli-tasks/index.js
+++ cli-tasks/index.js
@@ -5,13 +5,65 @@
 
 'use strict';
 
-// IMPORTS //
+// IMPORTS /
 
-const Listr = require('listr');
+// const Listr = require('listr');
+
+const taskz = require('taskz');
 
 // CONSTANTS //
 
-const tasks = new Listr([
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const tasks = taskz(
+    [
+        {
+            text: 'first task',
+            task: async () => {
+                await sleep(2000);
+            }
+        },
+        {
+            text: 'second task',
+            task: async () => {
+                await sleep(3000);
+            }
+        },
+        {
+            text: 'third task',
+            task: async () => {
+                await sleep(4000);
+            }
+        },
+        {
+            text: 'this task will fail and continue',
+            task: async () => {
+                await sleep(2500);
+                throw new Error('this task failed');
+            }
+        },
+        {
+            text: 'start running sub tasks',
+            tasks: taskz(
+                [
+                    {
+                        text: 'my subtask 1',
+                        task: async () => {
+                            await sleep(1000);
+                        }
+                    }
+                ],
+                { parallel: true }
+            )
+        }
+    ],
+    { parallel: true }
+);
+
+/*
+const listr = new Listr([
     {
         title: 'Success',
         task: () => 'Foo'
@@ -23,15 +75,20 @@ const tasks = new Listr([
         }
     }
 ]);
+*/
 
 // FUNCTIONS //
 
 (function () {
     console.log('\n --- CLI TASKS --- \n');
+    /*
     console.log('\n ----- LISTR ----- \n');
-    tasks.run().catch(err => {
+    listr.run().catch(err => {
         console.error(err);
     });
+    */
+    console.log('\n ----- TASKZ ----- \n');
+    tasks.run();
 })();
 
 // EOF //
