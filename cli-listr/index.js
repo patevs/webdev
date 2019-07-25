52f362235fe4271ef828ca2a190dc4ca2c73532c
PatEvs <ppevans11@gmail.com>
2019-07-25 19:13:30 +1200

    ✨  Updates to index.js

--- cli-listr/index.js
+++ cli-listr/index.js
@@ -3,8 +3,26 @@
  *  `cli-listr/index.js`
  */
 
+const Listr = require('listr');
+
+const tasks = new Listr([
+    {
+        title: 'Success',
+        task: () => 'Foo'
+    },
+    {
+        title: 'Failure',
+        task: () => {
+            throw new Error('Bar')
+        }
+    }
+]);
+
 (function () {
     console.log('\n --- CLI LISTR --- \n');
+    tasks.run().catch(err => {
+        console.error(err);
+    });
 })();
 
 // EOF //
