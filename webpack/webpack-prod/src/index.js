0639397532d39bd4ad6cdfa624340e6d82fa1eb3
patevs <ppevans11@gmail.com>
2019-10-19 11:27:54 +1300

    ✨  Updates

--- webpack/webpack-prod/src/index.js
+++ webpack/webpack-prod/src/index.js
@@ -4,12 +4,18 @@
 
 import _ from "lodash";
 import './style.css';
+import Logo from './logo.png';
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myLogo = new Image();
+  myLogo.src = Logo;
+  element.appendChild(myLogo);
 
   return element;
 }
