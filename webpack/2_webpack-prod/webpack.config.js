e9b817b678b759370b84fd09a979cc1562a9248b
patevs <ppevans11@gmail.com>
2019-10-19 12:18:59 +1300

    ✨  Updates

--- webpack/2_webpack-prod/webpack.config.js
+++ webpack/2_webpack-prod/webpack.config.js
@@ -1,27 +1,35 @@
 
 /**
- * `webpack.common.js`
+ * `webpack.config.js`
  */
 
-const path = require('path');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
+    mode: 'development',
     entry: {
         app: './src/index.js',
         print: './src/print.js',
     },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
     plugins: [
         // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
-        // new CleanWebpackPlugin(),
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-        title: 'Webpack Production',
+            title: 'Webpack Development',
         }),
     ],
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, "dist"),
+        publicPath: '/',
     },
     module: {
         rules: [
