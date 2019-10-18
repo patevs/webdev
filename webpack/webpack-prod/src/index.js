fda8bbaa0f9c5ee63e98014ebea4813161539125
patevs <ppevans11@gmail.com>
2019-10-19 11:13:28 +1300

    ✨  webpack-prod

---
+++ webpack/webpack-prod/src/index.js
@@ -0,0 +1,18 @@
+/**
+ * `src/index.js`
+ */
+
+import _ from "lodash";
+
+function component() {
+  const element = document.createElement("div");
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+/* EOF */
