562cc0bba285f930c089b69636def5808cdab4c2
patevs <ppevans11@gmail.com>
2019-10-19 12:14:34 +1300

    ✨  Updates

---
+++ webpack/2_webpack-prod/webpack.common.js
@@ -0,0 +1,27 @@
+
+/**
+ * `webpack.common.js`
+ */
+
+const path = require('path');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+    },
+    plugins: [
+        // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+        // new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+        title: 'Production',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
+
+/* EOF */
---
+++ webpack/2_webpack-prod/webpack.prod.js
@@ -0,0 +1,13 @@
+
+/**
+ * `webpack.prod.js`
+ */
+
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    mode: 'production',
+});
+
+/* EOF */
