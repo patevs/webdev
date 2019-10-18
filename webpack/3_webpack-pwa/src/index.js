9d2f984863cd7316125bc612e447f2fa5ea370f9
patevs <ppevans11@gmail.com>
2019-10-19 12:29:11 +1300

    ✨  Updates

--- webpack/3_webpack-pwa/src/index.js
+++ webpack/3_webpack-pwa/src/index.js
@@ -12,6 +12,16 @@ if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
 }
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
+
 function component() {
   const element = document.createElement("div");
   const btn = document.createElement('button');
