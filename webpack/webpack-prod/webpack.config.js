139a24d90f98b437cdc1036fb165148269216d73
patevs <ppevans11@gmail.com>
2019-10-19 11:35:08 +1300

    ✨  Updates

--- webpack/webpack-prod/webpack.config.js
+++ webpack/webpack-prod/webpack.config.js
@@ -6,9 +6,12 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js',
+    },
     output: {
-        filename: "bundle.js",
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, "dist"),
     },
     module: {
