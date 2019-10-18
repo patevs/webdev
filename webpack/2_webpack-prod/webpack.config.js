911f2ada38a0aaa25ef695c54faf0507817ad783
patevs <ppevans11@gmail.com>
2019-10-19 12:03:15 +1300

    ✨  Updates

--- webpack/2_webpack-prod/webpack.config.js
+++ webpack/2_webpack-prod/webpack.config.js
@@ -7,7 +7,7 @@ const path = require("path");
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -21,7 +21,7 @@ module.exports = {
     },
     plugins: [
         // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
-        new CleanWebpackPlugin(),
+        // new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             title: 'Webpack Development',
         }),
