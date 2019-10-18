8e516b771040613eb91a3dccad1f620236ba2788
patevs <ppevans11@gmail.com>
2019-10-19 11:41:48 +1300

    ✨  Updates

--- webpack/webpack-dev/webpack.config.js
+++ webpack/webpack-dev/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: 'Output Management',
+            title: 'Webpack Development',
         }),
     ],
     output: {
