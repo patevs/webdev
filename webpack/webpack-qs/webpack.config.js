51ef73292daea587fdbbc12fc450bcaa07f58226
patevs <ppevans11@gmail.com>
2019-10-19 10:46:51 +1300

    ✨  Updates

---
+++ webpack/webpack-qs/webpack.config.js
@@ -0,0 +1,16 @@
+
+/**
+ * `webpack.config.js`
+ */
+
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+};
+
+/* EOF */
